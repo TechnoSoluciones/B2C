ae388692d3c82ba44babd290b83544a0996cfe32
inssi <inssi@ASUS>
2016-02-11 17:46:55 -0500

    Creación clase usuarios.

--- modulos/usuarios/librerias/Usuarios_Usuarios.class.php
+++ modulos/usuarios/librerias/Usuarios_Usuarios.class.php
@@ -1,8 +1,60 @@
 <?php
 
-  /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  $raiz=isset($raiz)?$raiz:"../../../";
+  require_once($raiz."modulos/usuarios/Configuracion.cnf.php");
 
+  class Usuarios_Usuarios{
+
+      function crear($datos){
+          $db =new MySQL();
+          $sql="INSERT INTO `".$configuracion["empresa"]."_usuarios_usuarios` SET "
+              ."`usuario`='".$datos['usuario']."',"
+              ."`alias`='".$datos['alias']."',"
+              ."`clave`='".$datos['clave']."',"
+              ."`fecha`='".$datos['fecha']."',"
+              ."`hora`='".$datos['hora']."',"
+              ."`estado`='".$datos['estado']."'"
+              .";";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function actualizar($usuario,$campo,$valor){
+          $db =new MySQL();
+          $sql="UPDATE `".$configuracion["empresa"]."_usuarios_usuarios` "
+              ."SET `".$campo."`='".$valor."' "
+              ."WHERE `usuario`='".$usuario."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function eliminar($usuario){
+          $db =new MySQL();
+          $sql="DELETE FROM `".$configuracion["empresa"]."_usuarios_usuarios` "
+              ."WHERE `usuario`='".$usuario."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function consultar($usuario){
+          $db      =new MySQL();
+          $sql     ="SELECT * FROM `".$configuracion["empresa"]."_usuarios_usuarios` "
+              ."WHERE `usuario`='".$usuario."';";
+          $consulta=$db->sql_query($sql);
+          $fila    =$db->sql_fetchrow($consulta);
+          $db->sql_close();
+          return($fila);
+      }
+
+      function conteo(){
+          $db      =new MySQL();
+          $sql     ="SELECT COUNT(*) AS `conteo` FROM `".$configuracion["empresa"]."_usuarios_usuarios`;";
+          $consulta=$db->sql_query($sql);
+          $fila    =$db->sql_fetchrow($consulta);
+          $db->sql_close();
+          return($fila["conteo"]);
+      }
+
+  }
+
+?>
