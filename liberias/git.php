c07d656a2fbc8d1405ac9e0d82760fcc12457fc4
inssi <inssi@ASUS>
2016-02-11 16:50:20 -0500

    cambios

---
+++ liberias/git.php
@@ -0,0 +1,14 @@
+[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+	symlinks = false
+	ignorecase = true
+	hideDotFiles = dotGitOnly
+[remote "origin"]
+	url = https://jalexiscv@github.com/TechnoSoluciones/B2C.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "omega"]
+	remote = origin
+	merge = refs/heads/master
