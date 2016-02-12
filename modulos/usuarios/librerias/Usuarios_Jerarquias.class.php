0bad20c0906782232665b432aab5ae380fed54e6
inssi <inssi@ASUS>
2016-02-12 16:57:25 -0500

    jeranquias

---
+++ modulos/usuarios/librerias/Usuarios_Jerarquias.class.php
@@ -0,0 +1,46 @@
+<?php
+
+  class Usuarios_jerarquias{
+
+      function crear($datos){
+          $db =new MySQL();
+          $sql="INSERT INTO `".$configuracion["empresa"]."usuarios_jerarquias` SET "
+              ."`usuario`='".$datos['usuario']."',"
+              ."`rol`='".$datos['rol']."',"
+              ."`fecha`='".$datos['fecha']."',"
+              ."`hora`='".$datos['hora']."',"
+              ."`creador`='".$datos['creador']."'"
+              .";";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function actualizar($usuario,$campo,$valor){
+          $db =new MySQL();
+          $sql="UPDATE `".$configuracion["empresa"]."usuarios_jerarquias` "
+              ."SET `".$campo."`='".$valor."' "
+              ."WHERE `usuario`='".$usuario."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function eliminar($usuario){
+          $db =new MySQL();
+          $sql="DELETE FROM `".$configuracion["empresa"]."usuarios_jerarquias` "
+              ."WHERE `usuario`='".$usuario."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function consultar($usuario){
+          $db      =new MySQL();
+          $sql     ="SELECT * FROM `".$configuracion["empresa"]."usuarios_jerarquias` "
+              ."WHERE `usuario`='".$usuario."';";
+          $consulta=$db->sql_query($sql);
+          $fila    =$db->sql_fetchrow($consulta);
+          $db->sql_close();
+          return($fila);
+      }
+
+  }
+  ?>
