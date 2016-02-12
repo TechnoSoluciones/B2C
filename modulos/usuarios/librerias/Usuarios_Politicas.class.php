8687781699ac27426075cb0b06970b6bf80f580a
inssi <inssi@ASUS>
2016-02-12 17:01:02 -0500

    Politicas

--- modulos/usuarios/librerias/Usuarios_Politicas.class.php
+++ modulos/usuarios/librerias/Usuarios_Politicas.class.php
@@ -1,8 +1,50 @@
 <?php
 
-  /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  $raiz=isset($raiz)?$raiz:"../../../";
+  require_once($raiz."modulos/usuarios/Configuracion.cnf.php");
 
+  class Usuarios_Politicas{
+
+      function crear($datos){
+          $db =new MySQL();
+          $sql="INSERT INTO `".$configuracion["empresa"]."_usuarios_politicas` SET "
+              ."`rol`='".$datos['rol']."',"
+              ."`permiso`='".$datos['permiso']."',"
+              ."`fecha`='".$datos['fecha']."',"
+              ."`hora`='".$datos['hora']."',"
+              ."`creador`='".$datos['creador']."'"
+              .";";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function actualizar($rol,$campo,$valor){
+          $db =new MySQL();
+          $sql="UPDATE `".$configuracion["empresa"]."_politicas` "
+              ."SET `".$campo."`='".$valor."' "
+              ."WHERE `rol`='".$rol."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function eliminar($rol){
+          $db =new MySQL();
+          $sql="DELETE FROM `".$configuracion["empresa"]."_politicas` "
+              ."WHERE `rol`='".$rol."';";
+          $db->sql_query($sql);
+          $db->sql_close();
+      }
+
+      function consultar($rol){
+          $db      =new MySQL();
+          $sql     ="SELECT * FROM `".$configuracion["empresa"]."_politicas` "
+              ."WHERE `rol`='".$rol."';";
+          $consulta=$db->sql_query($sql);
+          $fila    =$db->sql_fetchrow($consulta);
+          $db->sql_close();
+          return($fila);
+      }
+
+  }
+
+?>
